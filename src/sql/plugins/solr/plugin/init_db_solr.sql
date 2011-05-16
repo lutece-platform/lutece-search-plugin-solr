09f831a0567200c3d301bc000c66e168c79ba810
RMA <RMA@bab10101-e421-0410-a517-8ce0973de3ef>
2011-05-16 15:38:41 +0000

    SOLR-21 : Spelling correction in the init SQL script
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-solr@29744 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/solr/plugin/init_db_solr.sql
+++ src/sql/plugins/solr/plugin/init_db_solr.sql
@@ -11,7 +11,7 @@ INSERT INTO solr_fields (id_field,name,label,description,is_facet,enable_facet,i
 (3,'date','Date','La date de creation du document',true,true,true,true,false);
 
 INSERT INTO solr_fields (id_field,name,label,description,is_facet,enable_facet,is_sort,enable_sort,default_sort) VALUES 
-(4,'score','Score','Pertinance du résultat',false,false,true,true,true);
+(4,'score','Score','Pertinence du résultat',false,false,true,true,true);
 
 INSERT INTO solr_fields (id_field,name,label,description,is_facet,enable_facet,is_sort,enable_sort,default_sort) VALUES 
 (5,'categorie','Categorie','La categorie du document',true,true,false,false,false);
