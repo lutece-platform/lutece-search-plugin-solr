6f27ab6b9c0a2f7cd336d82b34a003e95975af46
alexandre MANGOT <alexandre.mangot@atos.net>
2015-06-02 16:55:44 +0200

    SOLR-60 : change to colalted results : more efficient

--- src/java/fr/paris/lutece/plugins/search/solr/web/SolrSearchApp.java
+++ src/java/fr/paris/lutece/plugins/search/solr/web/SolrSearchApp.java
@@ -222,7 +222,7 @@ public class SolrSearchApp implements XPageApplication
         strCurrentPageIndex = (strCurrentPageIndex != null) ? strCurrentPageIndex : DEFAULT_PAGE_INDEX;
 
         SolrSearchEngine engine = SolrSearchEngine.getInstance();
-        SolrFacetedResult facetedResult = engine.getFacetedSearchResults(strQuery, facetQuery, sort, order, nLimit, Integer.parseInt(strCurrentPageIndex), nItemsPerPage);
+        SolrFacetedResult facetedResult = engine.getFacetedSearchResults(strQuery, facetQuery, sort, order, nLimit, Integer.parseInt(strCurrentPageIndex), nItemsPerPage, SOLR_SPELLCHECK);
         List<SolrSearchResult> listResults = facetedResult.getSolrSearchResults();
 
         // The page should not be added to the cache
@@ -269,7 +269,7 @@ public class SolrSearchApp implements XPageApplication
 
             if (checkResponse != null)
             {
-                model.put(MARK_SUGGESTION, checkResponse.getCollatedResult());
+                model.put(MARK_SUGGESTION, facetedResult.getSolrSpellCheckResponse().getCollatedResults());
             }
         }
 
