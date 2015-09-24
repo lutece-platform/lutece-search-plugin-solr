875ea00e98618d1dee05351c5d6a5cb5f9ed44b5
alexandre MANGOT <alexandre.mangot@atos.net>
2015-09-24 16:02:38 +0200

    SOLR-59 : add insert for major fields in sql update script

--- src/sql/plugins/solr/upgrade/update_db_solr_3.0.1-3.1.0.sql
+++ src/sql/plugins/solr/upgrade/update_db_solr_3.0.1-3.1.0.sql
@@ -1,3 +1,5 @@
-ALTER TABLE solr_fields ADD weight float DEFAULT 1;
 ALTER TABLE solr_fields ADD facet_mincount int DEFAULT 1;
 ALTER TABLE solr_fields ADD operator_type  varchar(30) DEFAULT 'OR';
+INSERT INTO solr_fields (name, label, description, weight, facet_mincount, operator_type) VALUES("title", "title" , "Title", 10, 1, "AND");
+INSERT INTO solr_fields (name, label, description, weight, facet_mincount, operator_type) VALUES("content", "content" , "Content", 1, 1, "AND");
+INSERT INTO solr_fields (name, label, description, weight, facet_mincount, operator_type) VALUES("summary", "summary" , "Summary", 1, 1, "AND");
