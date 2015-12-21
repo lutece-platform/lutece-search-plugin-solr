91266cadc5510d086d9aeeae4e2c5730b0fe8515
Isabelle Lu <isabelle.lu@atos.net>
2015-12-21 17:05:58 +0100

    SOLR-78 : Fix SQL scripts

--- src/sql/plugins/solr/upgrade/update_db_solr_3.0.1-3.1.0.sql
+++ src/sql/plugins/solr/upgrade/update_db_solr_3.0.1-3.1.0.sql
@@ -1,5 +1,7 @@
 ALTER TABLE solr_fields ADD facet_mincount int DEFAULT 1;
 ALTER TABLE solr_fields ADD operator_type  varchar(30) DEFAULT 'OR';
-INSERT INTO solr_fields (name, label, description, weight, facet_mincount, operator_type) VALUES("title", "title" , "Title", 10, 1, "AND");
-INSERT INTO solr_fields (name, label, description, weight, facet_mincount, operator_type) VALUES("content", "content" , "Content", 1, 1, "AND");
-INSERT INTO solr_fields (name, label, description, weight, facet_mincount, operator_type) VALUES("summary", "summary" , "Summary", 1, 1, "AND");
+
+SELECT @max := MAX( id_field ) FROM solr_fields;
+INSERT INTO solr_fields (id_field, name, label, description, weight, facet_mincount, operator_type) VALUES(@max + 1, "title", "title", "Title", 10, 1, "AND");
+INSERT INTO solr_fields (id_field, name, label, description, weight, facet_mincount, operator_type) VALUES(@max + 2, "content", "content", "Content", 1, 1, "AND");
+INSERT INTO solr_fields (id_field, name, label, description, weight, facet_mincount, operator_type) VALUES(@max + 3, "summary", "summary", "Summary", 1, 1, "AND");
