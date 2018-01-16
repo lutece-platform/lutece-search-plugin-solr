1b49a36f6d34fab509f0fe21774681c244d131b6
Jon Harper <jon.harper87@gmail.com>
2018-01-16 18:44:35 +0100

    SOLR-103 : Remove french sentences in the default database init

--- src/sql/plugins/solr/plugin/init_db_solr.sql
+++ src/sql/plugins/solr/plugin/init_db_solr.sql
@@ -2,19 +2,19 @@
 -- Dumping data for table solr_facet
 --
 INSERT INTO solr_fields (id_field,name,label,description,is_facet,enable_facet,is_sort,enable_sort,default_sort) VALUES 
-(1,'site','Site','Le site d''appartenance du document ou page',true,true,false,false,false);
+(1,'site','Site','Site',true,true,false,false,false);
 
 INSERT INTO solr_fields (id_field,name,label,description,is_facet,enable_facet,is_sort,enable_sort,default_sort) VALUES 
-(2,'type','Type','Le type du document',true,true,false,false,false);
+(2,'type','Type','Type',true,true,false,false,false);
 
 INSERT INTO solr_fields (id_field,name,label,description,is_facet,enable_facet,is_sort,enable_sort,default_sort) VALUES 
-(3,'date','Date','La date de creation du document',true,true,true,true,false);
+(3,'date','Date','Date',true,true,true,true,false);
 
 INSERT INTO solr_fields (id_field,name,label,description,is_facet,enable_facet,is_sort,enable_sort,default_sort) VALUES 
-(4,'score','Score','Pertinence du résultat',false,false,true,true,true);
+(4,'score','Score','Score',false,false,true,true,true);
 
 INSERT INTO solr_fields (id_field,name,label,description,is_facet,enable_facet,is_sort,enable_sort,default_sort) VALUES 
-(5,'categorie','Categorie','La categorie du document',true,true,false,false,false);
+(5,'categorie','Categorie','Category',true,true,false,false,false);
 
 INSERT INTO solr_fields (id_field, name, label, description, weight, facet_mincount, operator_type) VALUES
 (6, 'title', 'title', 'Title', 10, 1, 'AND');
