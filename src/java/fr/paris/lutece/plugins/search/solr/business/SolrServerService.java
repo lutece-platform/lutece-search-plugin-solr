58fbf20d8386b88229132516970226d50f298ebc
vanhemsj <jbvanhems@gmail.com>
2021-06-09 08:45:44 +0200

    SOLR-122 : Add Basic Authentication

--- src/java/fr/paris/lutece/plugins/search/solr/business/SolrServerService.java
+++ src/java/fr/paris/lutece/plugins/search/solr/business/SolrServerService.java
@@ -33,9 +33,15 @@
  */
 package fr.paris.lutece.plugins.search.solr.business;
 
+import fr.paris.lutece.plugins.search.solr.indexer.Authentification.SolrPreemptiveAuthInterceptor;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.HttpClientUtil;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.common.params.ModifiableSolrParams;
 
 /**
  * This service provides an instance of SolrServer.
@@ -44,9 +50,13 @@ import org.apache.solr.client.solrj.impl.HttpSolrClient;
 public final class SolrServerService
 {
     private static final String PROPERTY_SOLR_SERVER_URL = "solr.server.address";
+    private static final String PROPERTY_SOLR_SERVER_LOGIN = "solr.server.login";
+    private static final String PROPERTY_SOLR_SERVER_PASSWORD = "solr.server.password";
     private static final String PROPERTY_SOLR_TIMEOUT = "solr.server.timeout";
     private static final String PROPERTY_SOLR_SOCKET_TIMEOUT = "solr.socket.timeout";
     private static final String SOLR_SERVER_URL = AppPropertiesService.getProperty( PROPERTY_SOLR_SERVER_URL );
+    private static final String SOLR_SERVER_LOGIN = AppPropertiesService.getProperty( PROPERTY_SOLR_SERVER_LOGIN );
+    private static final String SOLR_SERVER_PASSWORD = AppPropertiesService.getProperty( PROPERTY_SOLR_SERVER_PASSWORD );
     private static final int SOLR_CONNECTION_TIMEOUT = AppPropertiesService.getPropertyInt( PROPERTY_SOLR_TIMEOUT, 10000 );
     private static final int SOLR_SOCKET_TIMEOUT = AppPropertiesService.getPropertyInt( PROPERTY_SOLR_SOCKET_TIMEOUT, 60000 );
     private static SolrServerService _instance;
@@ -57,7 +67,7 @@ public final class SolrServerService
      */
     private SolrServerService( )
     {
-        _solrServer = createSolrServer( SOLR_SERVER_URL );
+        _solrServer = createSolrServer( );
     }
 
     /**
@@ -84,7 +94,7 @@ public final class SolrServerService
     {
         if ( _solrServer == null )
         {
-            _solrServer = createSolrServer( SOLR_SERVER_URL );
+            _solrServer = createSolrServer( );
         }
 
         return _solrServer;
@@ -93,12 +103,21 @@ public final class SolrServerService
     /**
      * Creates the SolrServer.
      * 
-     * @param strServerUrl
-     *            the Solr server url
      * @return the SolrServer.
      */
-    private SolrClient createSolrServer( String strServerUrl )
+    private SolrClient createSolrServer( )
     {
-        return new HttpSolrClient.Builder( strServerUrl ).withConnectionTimeout( SOLR_CONNECTION_TIMEOUT ).withSocketTimeout( SOLR_SOCKET_TIMEOUT ).build( );
+        ModifiableSolrParams params = new ModifiableSolrParams( );
+
+        if( StringUtils.isNotBlank( SOLR_SERVER_LOGIN ) ) 
+        {
+            params.add( HttpClientUtil.PROP_BASIC_AUTH_USER, SOLR_SERVER_LOGIN );
+            params.add( HttpClientUtil.PROP_BASIC_AUTH_PASS, SOLR_SERVER_PASSWORD );
+            HttpClientUtil.addRequestInterceptor( new SolrPreemptiveAuthInterceptor( ) );           
+        }
+
+        CloseableHttpClient httpClient = HttpClientUtil.createClient( params );
+
+        return new HttpSolrClient.Builder( SOLR_SERVER_URL ).withHttpClient( httpClient ).withConnectionTimeout( SOLR_CONNECTION_TIMEOUT ).withSocketTimeout( SOLR_SOCKET_TIMEOUT ).build( );
     }
 }
