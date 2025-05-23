db606f83764644e2374f1a0722b76a4e3f05bb26
HOHL <laurent.hohl@gmail.com>
2025-05-23 16:12:51 +0200

    LUT-27964 : Update templates to last themes version and small fixes and add defaul ticon to solr features

---
+++ src/sql/plugins/solr/upgrade/update_db_solr_4.0.2-4.0.6.sql
@@ -0,0 +1,3 @@
+UPDATE core_admin_right SET icon_url='ti ti-pencil-search' WHERE id_right='SOLR_FIELDS_MANAGEMENT';
+UPDATE core_admin_right SET icon_url='ti ti-settings-search' WHERE id_right='SOLR_CONFIGURATION_MANAGEMENT';
+UPDATE core_admin_right SET icon_url='ti ti-cloud-bolt' WHERE id_right='SOLR_INDEX_MANAGEMENT';
